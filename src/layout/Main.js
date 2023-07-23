7ad072a5506dc1ef97e209aea98bbaf8f109b7a2
SumiAkter2 <akteralisha36@gmail.com>
2023-07-23 09:27:49 +0600

    added Footer

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 
 const Main = () => {
     return (
         <div>
-            <Navbar>
+            <Navbar/>
                 <Outlet/>
-            </Navbar>
+            <Footer/>
         </div>
     );
 };
