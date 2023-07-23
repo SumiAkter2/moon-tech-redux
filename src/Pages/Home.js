d47497e6a20514851b4d371a7aeca603a80d4344
SumiAkter2 <akteralisha36@gmail.com>
2023-07-23 17:03:56 +0600

    added Instock ,AMD ,Intel button in home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,17 +5,33 @@ import { useSelector } from "react-redux";
 const Home = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    fetch("Products.json")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
+     fetch("http://localhost:5000/products")
+       .then((res) => res.json())
+       .then((data) => setProducts(data.data));
   }, []);
-    const state= useSelector((state)=>state)
-    console.log(state);
+    // const state= useSelector((state)=>state)
+   const activeClass = "text-white  bg-indigo-500 border-white";
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      {products.map((product) => (
-        <ProductCard key={product._id} product={product} />
-      ))}
+    <div className="max-w-7xl gap-14 mx-auto my-10">
+      <div className="mb-10 flex justify-end gap-5">
+        <button
+          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+        >
+          In Stock
+        </button>
+        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+          AMD
+        </button>
+        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+          Intel
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
